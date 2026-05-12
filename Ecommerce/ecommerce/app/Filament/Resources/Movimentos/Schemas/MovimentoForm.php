be6c389d1dcc895b30692dc2fe69250ddab3029f
Isabelle Andrade <isabelleandrade1109@gmail.com>
2026-05-12 16:52:58 -0300

    aula até 12.05

---
+++ Ecommerce/ecommerce/app/Filament/Resources/Movimentos/Schemas/MovimentoForm.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\Movimentos\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class MovimentoForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('produto_id')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+}
---
+++ Ecommerce/ecommerce/database/migrations/2026_05_12_174546_create_movimentos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movimentos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('produto_id')->constraint() ->cascadeOnDelete();
+            //migration que cria uma foreign key com comportamento automático de exclusão em cascata
+            //Exclusão em cascata -> se um registro da tabela produtos for excluído, todos
+            //os registros da tabela atual que referenciam esse produto_id também serão
+            //apagados automaticamente 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movimentos');
+    }
+};
